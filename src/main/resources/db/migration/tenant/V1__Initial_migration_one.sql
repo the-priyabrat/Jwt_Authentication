3b4dc16e4db643f0dbfb68557cf9668985f0f534
priyabrat <priyabratswain@interlandtech.com>
2026-08-26 17:01:23 +0530

    User geo location now trw=eated as independent table instead child component. Resolved cuncurrency problem. Refactored exception throwing for external api call. Per ip lock introduced

--- src/main/resources/db/migration/tenant/V1__Initial_migration_one.sql
+++ src/main/resources/db/migration/tenant/V1__Initial_migration_one.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS http_trace (
 CREATE TABLE IF NOT EXISTS user_geo_coordinate (
 	geo_location_id varchar(255) NOT NULL,
 	city varchar(255) NULL,
-	client_ip varchar(255) NULL,
+	client_ip varchar(255) NULL UNIQUE,
 	continent_code varchar(255) NULL,
 	continent_name varchar(255) NULL,
 	country varchar(255) NULL,
@@ -67,17 +67,17 @@ CREATE TABLE IF NOT EXISTS  log_trace (
 	trace_id varchar(255) NOT NULL,
 	"version" varchar(255) NULL,
 	incoming_time time(0) NULL,
-	coordinate_trace_id varchar(255) NULL,
+-- 	coordinate_trace_id varchar(255) NULL,
 	operation_status varchar(255) DEFAULT 'COMPLETE'::character varying NULL,
 	CONSTRAINT log_trace_event_type_check CHECK (((event_type)::text = ANY ((ARRAY['APPLICATION_LOG'::character varying, 'API_REQUEST'::character varying, 'EXCEPTION'::character varying, 'CUSTOM_EVENT'::character varying])::text[]))),
 	CONSTRAINT log_trace_exception_trace_id_key UNIQUE (exception_trace_id),
 	CONSTRAINT log_trace_http_trace_id_key UNIQUE (http_trace_id),
 	CONSTRAINT log_trace_level_check CHECK (((level)::text = ANY ((ARRAY['TRACE'::character varying, 'DEBUG'::character varying, 'INFO'::character varying, 'WARN'::character varying, 'ERROR'::character varying, 'FATAL'::character varying])::text[]))),
-	CONSTRAINT log_trace_pkey PRIMARY KEY (log_id, time_stamp, trace_id),
-	CONSTRAINT uk9crje0ys3yfm7v1pxo7i0r7v1 UNIQUE (coordinate_trace_id)
+	CONSTRAINT log_trace_pkey PRIMARY KEY (log_id, time_stamp, trace_id)
+--     ,CONSTRAINT uk9crje0ys3yfm7v1pxo7i0r7v1 UNIQUE (coordinate_trace_id)
 );
 
 ALTER TABLE log_trace ADD CONSTRAINT fk6sulwel397lykfnigdwoht263 FOREIGN KEY (exception_trace_id) REFERENCES exception_trace(exception_id);
 ALTER TABLE log_trace ADD CONSTRAINT fk9vxe8qbruagm17tfu3npg4xwi FOREIGN KEY (http_trace_id) REFERENCES http_trace(http_id);
-ALTER TABLE log_trace ADD CONSTRAINT fkediabh9rgpgyrbln73lhy3tkh FOREIGN KEY (coordinate_trace_id) REFERENCES user_geo_coordinate(geo_location_id);
+-- ALTER TABLE log_trace ADD CONSTRAINT fkediabh9rgpgyrbln73lhy3tkh FOREIGN KEY (coordinate_trace_id) REFERENCES user_geo_coordinate(geo_location_id);
 
